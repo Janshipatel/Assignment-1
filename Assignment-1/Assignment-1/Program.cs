3c24d4254ece158db14b3c449a549416ecb68f32
Janshipatel <108849194+Janshipatel@users.noreply.github.com>
2022-08-09 11:16:25 +0530

    Add files via upload

---
+++ Assignment-1/Assignment-1/Program.cs
@@ -0,0 +1,480 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Program 1:
+
+            //Console.WriteLine("hello");
+            //Console.WriteLine("janshi patel");
+
+            ////Program 2:
+
+            //Console.WriteLine("Enter x:");
+            //string x = Console.ReadLine();
+            //Console.WriteLine("x=" + x);
+            //Console.WriteLine("Enter y:");
+            //string y = Console.ReadLine();
+            //Console.WriteLine("y="+y);
+            //Console.WriteLine("z=x+y"+z);
+
+            ////Program 3:
+
+            //Console.WriteLine(10 / 2);
+ 
+            ////Program 4:
+
+            //Console.WriteLine(-1+4*6);
+            //Console.WriteLine((35+5)%7);
+            //Console.WriteLine(14+-4*6/11);
+            //Console.WriteLine(2+15/6*1-7%2);
+
+            ////Program 5:
+
+            //int a = 5;
+            //int b = 6;
+            //int x;
+            //Console.WriteLine("before swap");
+            //x=a;
+            //a=b;
+            //b=x;
+            //Console.Write("after swapping");
+            //Console.Write("\na :"+a);
+            //Console.Write("\nb :"+b);
+            //Console.Read();
+
+            ////Program 6:
+
+            //int x, y, z;
+            //Console.WriteLine("Enter x:");
+            //x = Convert.ToInt16(Console.ReadLine()); ;
+            //Console.WriteLine("Enter y:");
+            //y =  Convert.ToInt16(Console.ReadLine());;
+            //Console.WriteLine("Enter z:");
+            //z = Convert.ToInt16(Console.ReadLine());
+            //int o = x * y * z;
+            //Console.WriteLine("Output: {0} * {1} * {2} = {3}",x,y,z,o);
+
+            ////Program 7:
+
+            //int x, y;
+            //Console.WriteLine("enter x:");
+            //x = Convert.ToInt16(Console.ReadLine());
+            //Console.WriteLine("enter y:");
+            //y = Convert.ToInt16(Console.ReadLine());
+            //int z = x + y;
+            //Console.WriteLine("z : {0} + {1} = {2}", x, y,z);
+            //int p = x - y;
+            //Console.WriteLine("p : {0} - {1} = {2}", x, y, p);
+            //int q = x * y;
+            //Console.WriteLine("q : {0} * {1} = {2}", x, y, q);
+            //int r = x / y;
+            //Console.WriteLine("r : {0} / {1} = {2}", x, y, r);
+
+            ////Program 8:
+
+            //int i;
+            //int x;
+            //Console.WriteLine("enter x:");
+            //x = Convert.ToInt16(Console.ReadLine());
+            //Console.Write("\n");
+            //for (i=1;i<=10;i++)
+            //{
+            //    Console.Write("{0} * {1} = {2} \n", i, x, i * x);
+            //}
+
+            ////Program 9:
+
+            //int p, q, r, s;
+            //Console.WriteLine("Enter First number:");
+            //p = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter Second number:");
+            //q = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter third number:");
+            //r = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter fourth number:");
+            //s = Convert.ToInt32(Console.ReadLine());
+            //int z = p + q + r + s;
+            //Console.WriteLine("z:{0} {1} {2} {3} = {4}", p, q, r, s, z);
+            //int y = z / 4;
+            //Console.WriteLine(" Averege is y: {0} ", y);
+
+            ////Program 10:
+
+            //int x, y, z;
+            //Console.WriteLine("Enter x:");
+            //x = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter y:");
+            //y = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter z:");
+            //z = Convert.ToInt32(Console.ReadLine());
+            //int p = (x + y) * z;
+            //Console.WriteLine("sum of p: ({0}+{1})*{2}={3}",x,y,z,p);
+            //int q = (x * y) + (y * z);
+            //Console.WriteLine("sum of q: ({0}*{1})+({1}*{2})={3}",x,y,z,q);n
+
+            ////Program 11:
+
+            //int a;
+            //Console.WriteLine("enter your age:");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("You look older than: {0}",a);
+
+            ////Program 12:
+
+            //Console.WriteLine("Enter x:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i < 2; i++)
+            //{
+            //    for (int y=0;y < 4; y++)
+            //    {
+            //    Console.Write("{0} " ,x);
+            //    }
+            //Console.WriteLine();
+            //for (int k = 0; k < 4 ; k++)
+            //{
+            //    Console.Write("{0}" ,x);
+            //}
+            //Console.WriteLine();
+            //}      
+
+            ////Program 13:
+
+            //int length = 3, width = 5;
+            //Console.Write(" ");
+
+            //for (int i = 1; i <= width; i++)
+            //{
+            //    for (int j = 1; j <= length; j++)
+            //    {
+            //        if (i == 1 || i == width || j == 1 || j == length)
+            //        {
+            //            Console.Write("5");
+            //        }
+            //        else
+            //        {
+            //            Console.Write(" ");
+            //        }
+            //    }
+            //    Console.Write("\n ");
+            //}
+
+            //Program 14:
+
+            //Console.WriteLine("Enter celsius:");
+            //int c = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("kelvin = {0}",c +273);
+            //Console.WriteLine("fahrenheit = {0}",c*18/10+3);
+        
+            ////Program 15:
+
+            //string S = "Atmiya University";
+            //Console.WriteLine("{0}");
+            //Console.WriteLine("{0}",S.Remove(2,1));
+            //Console.WriteLine("{0}",S.Remove(7,1));
+
+            ////Program 16:
+
+            //Console.WriteLine(test("The quick brown fox jumps over the lazy dog"));
+            //}
+            //    public  static string test(string str)
+            //    {
+            //        var s = str.Substring(str.Length-1);
+            //        return s + str + s;
+            //    }
+            //}
+
+            ////Program 18:
+
+            //Console.WriteLine(a(1,2));
+            //Console.WriteLine(a(3, 2));
+            //Console.WriteLine(a(2, 2);
+            //Console.ReadLine();
+            //public static int a(int x, int y)
+            //{
+            //   return x == y ? (x + y) * 3 : x + y;
+            //}
+            //public static void main()
+            //{
+            //   string s = "Write C# Sharp Program to folowing pattern using the alphabet.";
+            //   Console.WriteLine(s.ToLower());
+            //}
+    
+            ////Program 19:
+
+            //int First, Second, Absolute= 0;
+            //Console.Write("Input first integer : ");
+            //First = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Input Second integer : ");
+            //Second = Convert.ToInt32(Console.ReadLine());
+            //if (First > Second)
+            //    Absolute = (First - Second) * 2;
+            //else
+            //    Absolute = Second - First;
+            //Console.WriteLine("the absolute value of the difference between two given numbers : {0}", Absolute);
+
+            ////Program 20:
+
+            //int First, Second, Sum = 0;
+            //Console.Write("Input first integer : ");
+            //First = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Input Second integer : ");
+            //Second = Convert.ToInt32(Console.ReadLine());
+            //Sum = First + Second;
+            //Console.WriteLine("the sum of the two given integers and return true if one of the integer is 20 or if their sum is 20.");
+            //if (Sum.Equals(First) || Sum.Equals(Second))
+            //    Console.WriteLine("True");
+            //else
+            //    Console.WriteLine("False");
+ 
+            ////Program 21:
+
+            //int Number;
+            //Console.Write("Input an integer : ");
+            //Number = Convert.ToInt32(Console.ReadLine());
+            //if(Math.Abs(Number - 100) <=20 || Math.Abs(Number - 200) <=20)
+            //    Console.WriteLine("True");
+            //else
+            //    Console.WriteLine("False");
+  
+            ////Program 22:
+
+            //String Str = "ATMIYA UNIVERSITY";
+            //Console.WriteLine("String : {0}",Str);
+            //Console.WriteLine("String into lowercase : {0}", Str.ToLower());
+   
+            ////Program 23:
+
+            //for (int i = 0; i <=99; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        Console.Write("{0} ", i);
+            //    }
+            //}
+  
+            ////Program 25:
+            //Console.Write("Input  a number(integer): ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+            //int sum = 0;
+            //while (n != 0)
+            //{
+            //    sum += n % 10;
+            //    n /= 10;
+            //}
+            //Console.WriteLine("Sum of the digits of the said integer: " + sum);
+
+            ////Program 26:
+
+            //String Str = " Display the pattern like pyramid using the alphabet.";
+            //Console.WriteLine("Original String : {0}", Str);
+
+            ////Program 28:
+
+            //string hexval;
+            //Console.Write("Enter the Hexadecimal code : ");
+            //hexval = Console.ReadLine();
+            //Console.WriteLine("Hexadecimal number: " + hexval);
+            //int decValue = int.Parse(hexval, System.Globalization.NumberStyles.HexNumber);
+            //Console.WriteLine("Convert to-");
+            //Console.WriteLine("Decimal number: " + decValue);
+
+            ////Program 29:
+            //int[] First_Array = { 1, 3, -5, 4 };
+            //int[] Seconde_Array = {1,4,-5,-2};
+            //for (int i = 0; i<First_Array.Length; i++)
+            //    {
+            //        Console.Write(First_Array[i] * Seconde_Array[i]+" ");
+            //    }
+ 
+            ////Program 30:
+
+            //string Str;
+            //Console.Write("Enter the String : ");
+            //Str = Console.ReadLine();
+            //if (Str.Length < 4)
+            //{
+            //    for (int i = 0; i < 4; i++)
+            //    {
+            //        Console.Write(Str);
+            //    }
+            //}
+            //else
+            //{
+            //    for (int i = 0; i < 4; i++)
+            //    {
+            //        Console.Write(Str.Substring(Str.Length-4));
+            //    }
+            //}
+
+            ////Program 31:
+
+            //Console.Write("Input first integer : ");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //if (x % 3 == 0 || x % 7 == 0)
+            //    Console.WriteLine("True");
+            //else
+            //    Console.WriteLine("False");
+
+            ////Program 32:
+
+            //String Str;
+            //Console.Write("Enter the string : ");
+            //Str = Console.ReadLine();
+            //if (Str.StartsWith("Hello"))
+            //    Console.WriteLine("True");
+            //else
+            //    Console.WriteLine("False");
+
+            ////Program 33:
+
+            //int First, Second;
+            //Console.Write("Input first integer : ");
+            //First = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Input Second integer : ");
+            //Second = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("two given numbers where one is less than 100 and other is greater than 200.");
+            //if (First < 100 && Second > 200)
+            //    Console.WriteLine("True");
+            //else
+            //    Console.WriteLine("False");
+    
+            ////Program 34:
+
+            //int First, Second;
+            //Console.Write("Input first integer : ");
+            //First = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Input Second integer : ");
+            //Second = Convert.ToInt32(Console.ReadLine());
+            //if ((First >= -10 && First <= 10) && (Second >= -10 && Second <=10 ))
+            //    Console.WriteLine("True");
+            //else
+            //    Console.WriteLine("False");
+
+            ////Program 35:
+
+            //string Str ;
+            //Console.Write("Enter the String : ");
+            //Str  = Console.ReadLine();
+            //Console.WriteLine((Str.Substring(1, 2).Equals("HP") ? Str.Remove(1, 2) : Str));
+   
+            ////Program 36:
+
+            //int First, Second,Third;
+            //Console.Write("Input first integer : ");
+            //First = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Input Second integer : ");
+            //Second = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Input Third integer : ");
+            //Third = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Largest of three: " + Math.Max(First, Math.Max(Second, Third)));
+            //Console.WriteLine("Lowest of three: " + Math.Min(First, Math.Min(Second, Third)));
+
+            ////Program 37:
+
+            //Console.WriteLine("Input first integer:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Input second integer:");
+            //int y = Convert.ToInt32(Console.ReadLine());
+            //int n = 20;
+            //var val1 = Math.Abs(x - n);
+            //var val2 = Math.Abs(y - n);
+            //Console.WriteLine("the nearest value of 20 of two given integers : {0}", val1 == val2 ? 0 : (val1 < val2 ? x : y));
+
+            ////Program 38:
+
+            //Console.WriteLine("Input an integer:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int[] nums = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 9 };
+            //Console.WriteLine("Number of " + x + " present in the said array:");
+            ////Console.WriteLine(nums.Count(n => n == x));
+
+            ////Program 39:
+
+            //Console.WriteLine("\nInput an integer:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int[] nums = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 9 };
+            //Console.WriteLine((nums[0] == x) || (nums[nums.Length - 1] == x));
+
+            ////Program 40:
+
+            //int[] nums = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+            //Console.WriteLine("\nArray1: [{0}]", string.Join(", ", nums));
+            //var sum = 0;
+            //for (var i = 0; i < nums.Length; i++)
+            //{
+            //    sum += nums[i];
+            //}
+            //Console.WriteLine("Sum: " + sum);
+
+            ////Program 41:
+
+            //int[] nums1 = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1 };
+            //Console.WriteLine("\nArray1: [{0}]", string.Join(", ", nums1));
+            //int[] nums2 = { 1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 5 };
+            //Console.WriteLine("\nArray2: [{0}]", string.Join(", ", nums2));
+
+            //Console.WriteLine("\nCheck if the first element or the last element of the two arrays ( length 1 or more) are equal.");
+            //Console.WriteLine((nums1[0].Equals(nums2[0])) || (nums1[nums1.Length - 1].Equals(nums2[nums2.Length - 1])));
+
+            ////Program 42:
+
+            //int[] nums = { 1, 2, 8 };
+            //Console.WriteLine("\nArray1: [{0}]", string.Join(", ", nums));
+            //var temp = nums[0];
+            //for (var i = 0; i < nums.Length - 1; i++)
+            //{
+            //    nums[i] = nums[i + 1];
+            //}
+            //nums[nums.Length - 1] = temp;
+            //Console.WriteLine("\nAfter rotating array becomes: [{0}]", string.Join(", ", nums));
+
+            ////Program 43:
+
+            //int[] nums = { 1, 2, 5, 7, 8 };
+            //Console.WriteLine("\nArray1: [{0}]", string.Join(", ", nums));
+            //var h_val = nums[0];
+            //for (var i = 0; i < nums.Length; i++)
+            //{
+            //    if (nums[i] > nums[0])
+            //        h_val = nums[i];
+            //}
+            //Console.WriteLine("\nHighest value between first and last values of the said array: {0}", h_val);
+
+            ////Program 44:
+
+            //int[] array1 = { 1, 2, 5 };
+            //Console.WriteLine("\nArray1: [{0}]", string.Join(", ", array1));
+            //int[] array2 = { 0, 3, 8 };
+            //Console.WriteLine("\nArray2: [{0}]", string.Join(", ", array2));
+            //int[] array3 = { -1, 0, 2 };
+            //Console.WriteLine("\nArray3: [{0}]", string.Join(", ", array3));
+            //int[] new_array = { array1[1], array2[1], array3[1] };
+            //Console.WriteLine("\nNew array: [{0}]", string.Join(", ", new_array));
+
+            ////Program 45:
+
+            //int[] Array = { 2, 4, 7, 8, 6 };
+            //foreach (var n in Array)
+            //{
+            //    if (n % 2 != 0)
+            //    {
+            //        Console.WriteLine("True");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("False");
+            //    }
+            //}
+
+
+        }
+    }
+}
